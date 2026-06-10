9678aeb16b049c1f9c2e8e9463ffb7edd13c90b0
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2026-06-10 00:16:23 -0400

    Love is the delusion that one woman differs from another. 		-- H.L. Mencken

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -46,7 +46,7 @@ if status is-interactive
 
     # Fastfetch (System Info)
     if type -q fastfetch
-        fastfetch --config koda.jsonc
+        fastfetch --config config.jsonc
     end
     abbr --add fast fastfetch
 
